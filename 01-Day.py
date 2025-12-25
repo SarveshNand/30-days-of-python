9bb2cd984d23c496ae500b7294e34287e02f8d03
SarveshNand <tensaibaka007@gmail.com>
2025-12-25 10:59:53 +0530

    Complete day 1 and 2 of 30 day of Python

---
+++ 01-Day.py
@@ -0,0 +1,22 @@
+# Introduction
+
+print(f"Addition: {3 + 2}")    # addition
+print(f"Subtraction: {3 - 2}")    # subtraction
+print(f"Multiplication: {3 * 2}")    # multiplication
+print(f"Division: {3 / 2}")    # division
+print(f"Exponent: {3 ** 2}")   # exponent
+print(f"Modulus: {3 % 2}")    # modulus
+print(f"Floor division: {3 // 2}")   # floor division operator
+
+# Checking Data Types
+
+print(type(10))                     # int
+print(type(3.14))                   # float
+print(type(1 + 3j))                 # complex
+print(type('Sarvesh'))              # str
+print(type([1, 2, 3]))              # list
+print(type({'name': 'Sarvesh'}))    # dict
+print(type({9.8, 3.14, 2.7}))       # set
+print(type((9.8, 3.14, 2.7)))       # tuple
+print(type(3 == 3))                 # Bool (True)
+print(type(3 >= 3))                 # Bool (False)
---
+++ 02-day/variables.py
@@ -0,0 +1,63 @@
+# Day 2: 30 Days of python programming
+
+# Exercise: Level 1
+
+
+first_name = 'Sarvesh'
+last_name = 'Nand'
+full_name = first_name + ' ' + last_name
+country = 'India'
+city = 'Jaunpur'
+age = 20
+year = 2025
+is_married = False
+is_true = True
+is_light_on = True
+
+listen_music, watch_anime, read_books, is_extreme_extrovert = True, True, True, False
+
+# Exercise: Level 2
+
+
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(type(listen_music))
+print(type(watch_anime))
+print(type(read_books))
+print(type(is_extreme_extrovert))
+
+print(len(first_name))
+print(f"Length of First name: {len(first_name)}, Length of Last name: {len(last_name)}")
+
+num_one = 5
+num_two = 4
+total = num_one + num_two
+
+diff = num_one - num_two
+product = num_two * num_one
+division = num_one / num_two
+remainder = num_two % num_one
+exp = num_one ** num_two
+floor_division = num_one // num_two
+
+radius_of_circle = 30
+area_of_circle = 3.14 * (radius_of_circle ** 2)
+circum_of_circle = 2 * 3.14 * radius_of_circle
+radius_of_circle = int(input("Enter the radius of the circle: "))
+area_of_circle = 3.14 * (radius_of_circle ** 2)
+
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+country = input("Enter your country: ")
+age = int(input("Enter your age: "))
+
+help('keywords')
